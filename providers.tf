d48e22adae1197409444d5d6c522ec7935a861a8
Nemanja Mitrovic <45688030+slashsd@users.noreply.github.com>
2025-12-04 14:36:04 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 1.0.0"
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -15,17 +15,15 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
-    default_tags {
+  default_tags {
     tags = {
       ManagedBy = "HCP Terraform"
     }
   }
 }
 
-# Confirue AAP Provider
+# Configure AAP Provider
 provider "aap" {
-  host = "https://caap.fvz.ansible-labs.de" # Also supports AAP_HOSTNAME environment variable
-
-  # Token authentication is recommended
-  token = "a480507ea1bbf6e967348d47cd969a0c147422ea" # Also supports AAP_TOKEN environment variable
+  host  = "https://caap.fvz.ansible-labs.de" # Also supports AAP_HOSTNAME environment variable
+  token = var.aap_token # Use a variable or environment variable
 }
