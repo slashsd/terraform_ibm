76f20cb79e8739252c12974b2da6b666477be2ff
Nemanja Mitrovic <45688030+slashsd@users.noreply.github.com>
2025-12-04 14:13:56 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -6,6 +6,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    aap = {
+      source = "ansible/aap"
+    }
   }
 }
 
@@ -19,4 +22,10 @@ provider "aws" {
   }
 }
 
+# Confirue AAP Provider
+provider "aap" {
+  host = "https://caap.fvz.ansible-labs.de" # Also supports AAP_HOSTNAME environment variable
 
+  # Token authentication is recommended
+  token = "a480507ea1bbf6e967348d47cd969a0c147422ea" # Also supports AAP_TOKEN environment variable
+}
