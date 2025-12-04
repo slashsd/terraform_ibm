4a38389d579b7583c7b8f97f2ec19f6b56f8d9f1
Nemanja Mitrovic <45688030+slashsd@users.noreply.github.com>
2025-12-04 14:50:35 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -25,5 +25,5 @@ provider "aws" {
 # Configure AAP Provider
 provider "aap" {
   host  = "https://caap.fvz.ansible-labs.de" # Also supports AAP_HOSTNAME environment variable
-  token = var.aap_token # Use a variable or environment variable
+  #token = var.aap_token # Use a variable or environment variable
 }
